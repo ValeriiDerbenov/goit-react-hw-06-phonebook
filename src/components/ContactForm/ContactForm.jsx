4885a4bf3b59db314c7425f0a9d375d59e9f57b2
Valerii <vd.alternative@gmail.com>
2024-02-02 23:42:49 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 import { Button, Form } from "./ContactForm.styled";
-import { useDispatch } from "react-redux";
 
 const ContactForm = ({ onSubmit }) => {
-  const dispatch = useDispatch();
-
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
