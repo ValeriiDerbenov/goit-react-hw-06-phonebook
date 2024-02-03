ff1aeb3a3bda128a4ba766caf6f565e923a267da
Valerii <vd.alternative@gmail.com>
2024-02-03 15:23:50 +0200

    contact form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,56 +12,14 @@ import ContactForm from "./ContactForm/ContactForm";
 export const App = () => {
    
   const contacts = useSelector(state => state.contactsStore.contacts);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = contact => {
-    const isInContacts = contacts.some(
-      ({ name }) =>
-        name.toLowerCase().trim() === contact.name.toLowerCase().trim()
-    );
-   
-    if (isInContacts) {
-      alert(`${contact.name} is already in contacts`);
-      return;
-    }
-
-    // setContacts(prevContacts => [
-    //   ...prevContacts,
-    //   { id: nanoid(), ...contact },
-    // ]);
-  };
-
-  const changeFilter = event => setFilter(event.target.value.trim());
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  // const removeContact = contactId => setContacts(prevContacts =>
-  //     prevContacts.filter(contact => contact.id !== contactId));
-
-  const visibleContacts = getVisibleContacts();
-
+  
   return (
     <section>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContact} />
-      <h2>Contacts</h2>
-      {contacts.length > 0 ? (<Filter value={filter} onChangeFilter={changeFilter} />) : (<p>Your phonebook is empty. Add first contact!</p>)}
-      {contacts.length > 0 && (
-          <ContactList
-          contacts={visibleContacts}
-          // onRemoveContact={removeContact}
-        />
-      )}
+      <ContactForm />
+      <h2>Contacts</h2> 
+      <Filter />
+      {contacts.length > 0 && <ContactList />}
     </section>
   );
 };
